852fa03db03fc585098e00492949308bca9c380e
Tanner Robinson <Rob16041@byui.edu>
2022-09-29 10:34:45 -0600

    Added navigation functionality and Documents comp

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
 
 @Component({
@@ -8,9 +8,9 @@ import { Contact } from '../contact.model';
 })
 export class ContactDetailComponent implements OnInit {
 
+ @Input() contact: Contact;
+ 
   constructor() { }
-
-  Contact: Contact = new Contact("1","R. Kent Jackson","jacksonk@byui.edu","208-496-3771", "../../assets/images/jacksonk.jpg",null);
 
   ngOnInit(): void {
   }
