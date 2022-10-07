47139e07ffc57b435155e289f6cac2fd05071cef
Tanner Robinson <Rob16041@byui.edu>
2022-10-06 18:11:13 -0600

    Adding Documents to system

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+
+  onSelected(selectedDocument:Document) {
+    //console.log(selectedDocument);
+    this.selectedDocumentEvent.emit(selectedDocument);
+  }
+
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document('1', 'New Document', 'Description', 'www.google.com', null),
+    new Document('2', 'The Cat in the Hat', 'A cat in a hat', 'www.suess.com', null),
+    new Document('3', 'CMS Requirements', 'CSM system', 'www.cms.com', null)
+  ]
 
   constructor() { }
 
